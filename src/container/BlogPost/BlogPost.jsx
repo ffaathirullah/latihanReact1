e170d4c6d7e072748015bd186cba4833707b9b95
Fachrul_Faathirullah <lenpuchongloi93@gmail.com>
2021-08-12 21:41:13 +0700

    panggil api menggunakan axios

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -1,18 +1,24 @@
 import React, { Component, Fragment } from "react";
 import Post from "./../../component/Post/Post";
+import axios from "axios";
 
 class BlogPost extends Component {
   state = {
     post: [],
   };
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((json) => {
-        this.setState({
-          post: json,
-        });
+    // fetch("https://jsonplaceholder.typicode.com/posts")
+    //   .then((response) => response.json())
+    //   .then((json) => {
+    //     this.setState({
+    //       post: json,
+    //     });
+    //   });
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((result) => {
+      this.setState({
+        post: result.data,
       });
+    });
   }
   render() {
     return (
