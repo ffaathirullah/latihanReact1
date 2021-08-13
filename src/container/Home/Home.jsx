2b032b99e9988d1be5c9b9191fbd5f796b0f4fed
Fachrul_Faathirullah <lenpuchongloi93@gmail.com>
2021-08-13 16:54:28 +0700

    react router DOM

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -2,17 +2,34 @@ import React, { Component, Fragment } from "react";
 import YoutubeComponent from "./../../component/YoutubeComponent/YoutubeComponent";
 import { Product } from "./../Product/Product";
 import BlogPost from "./../BlogPost/BlogPost";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class Home extends Component {
   render() {
     return (
       <div>
-        {/* <YoutubeComponent thumb="hallo 1" desc="desc 1" />
-        <YoutubeComponent thumb="hallo 2" desc="desc 2" />
-        <YoutubeComponent thumb="hallo 3" desc="desc 3" />
-        <YoutubeComponent desc="desc 4" /> */}
-        {/* <Product /> */}
-        <BlogPost />
+        <Router>
+          {/* <YoutubeComponent thumb="hallo 1" desc="desc 1" />
+          <YoutubeComponent thumb="hallo 2" desc="desc 2" />
+          <YoutubeComponent thumb="hallo 3" desc="desc 3" />
+          <YoutubeComponent desc="desc 4" />
+          <Product />
+          <BlogPost /> */}
+          <Fragment>
+            <div>
+              <Link to="/">Home</Link>
+              <Link to="/product">Home</Link>
+              <Link to="/blog">Blog</Link>
+            </div>
+          </Fragment>
+          <Route path="/" exact component={YoutubeComponent} />
+          <Route path="/product">
+            <Product />
+          </Route>
+          <Route path="/blog">
+            <BlogPost />
+          </Route>
+        </Router>
       </div>
     );
   }
