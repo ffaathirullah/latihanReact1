fcd576519735de6118d2eea54f147113d03505e7
Fachrul_Faathirullah <lenpuchongloi93@gmail.com>
2021-08-16 21:39:30 +0700

    sideffect dari react hooks

--- src/container/Hooks/Hooks.jsx
+++ src/container/Hooks/Hooks.jsx
@@ -1,7 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Hooks(props) {
   const [count, setCount] = useState(2);
+
+  useEffect(
+    () => {
+      document.title = `${count}`;
+      //compoent will unmount
+      return () => {
+        document.title = `back to component will unmount`;
+      };
+    }
+    //   , [input]
+  );
 
   return (
     <div>
